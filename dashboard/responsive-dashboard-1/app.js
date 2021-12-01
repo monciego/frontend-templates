d902073658be453dba90cb11273249bf918673dc
Jericho <jerichobantiquete@gmail.com>
2021-12-01 17:51:34 +0800

    user dropdown

--- dashboard/responsive-dashboard-1/app.js
+++ dashboard/responsive-dashboard-1/app.js
@@ -4,3 +4,11 @@ const mobileSearchInput = document.querySelector(".mobile-search");
 searchBtn.onclick = () => {
   mobileSearchInput.classList.toggle("active");
 };
+
+// user dropdowm
+const userDropDown = document.querySelector(".user-dropdown");
+const userDropDownContent = document.querySelector(".user-dropdown-content");
+
+userDropDown.addEventListener("click", () => {
+  userDropDownContent.classList.toggle("active");
+});
