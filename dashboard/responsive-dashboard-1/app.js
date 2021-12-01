b87b93f5f2c6bd6e2b1f075112869c6d68f84b9b
Jericho <jerichobantiquete@gmail.com>
2021-12-01 18:30:44 +0800

    added notifications dropdown content

--- dashboard/responsive-dashboard-1/app.js
+++ dashboard/responsive-dashboard-1/app.js
@@ -1,3 +1,4 @@
+// mobile search
 const searchBtn = document.querySelector(".search-icon");
 const mobileSearchInput = document.querySelector(".mobile-search");
 
@@ -5,10 +6,18 @@ searchBtn.onclick = () => {
   mobileSearchInput.classList.toggle("active");
 };
 
-// user dropdowm
+// user dropdown
 const userDropDown = document.querySelector(".user-dropdown");
 const userDropDownContent = document.querySelector(".user-dropdown-content");
 
 userDropDown.addEventListener("click", () => {
   userDropDownContent.classList.toggle("active");
+});
+
+// notification
+const notificationBtn = document.querySelector(".notifications");
+const notificationsContent = document.querySelector(".notifications-content");
+
+notificationBtn.addEventListener("click", () => {
+  notificationsContent.classList.toggle("active");
 });
